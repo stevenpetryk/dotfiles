206b7cb508a2074e61d2d7fceada3ecf317ebcad
Steven Petryk <petryk.steven@gmail.com>
2023-10-20 10:50:18 -0700

    Update fonts

--- fonts/gg.nix
+++ fonts/gg.nix
@@ -5,17 +5,16 @@ with pkgs;
 (stdenvNoCC.mkDerivation
 rec {
   pname = "gg-mono";
-  version = "1.8.0";
+  version = "20231018";
 
-  src = fetchFromGitHub {
-    owner = "Colophon-Foundry";
-    repo = "discord-text-type";
-    rev = "92ab008a1864e394fa9bbc72c4ef0e36a481abc6";
-    sha256 = "sha256-A9Uo7P8G8m+zehcHB2wgOfNW7iUsXzMXAUZ7sH7AM84=";
+  src = builtins.fetchGit {
+    url = "git@github.com:Colophon-Foundry/discord-text-type.git";
+    ref = "HEAD";
+    rev = "eea82d9006edf69c565d32a00374678044caf46c";
   };
 
   installPhase = ''
     mkdir -p $out/share/fonts/truetype
-    cp -r monospace/betas/20230712-v1.001/*.ttf $out/share/fonts/truetype
+    cp -r betas/20231018/*.ttf $out/share/fonts/truetype
   '';
 })
